2c4ab30242d75b3d88e891e0733a272d4ebe2d5d
Hiram-hr <118991748+Hiram-hr@users.noreply.github.com>
2023-11-26 18:02:21 -0600

    Create ASA.java
    mi parte

---
+++ ASA.java
@@ -0,0 +1,227 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cr.principal;
+import java.util.List;
+import java.util.Stack;
+
+import java.util.List;
+import java.util.Stack;
+
+public class ASA implements Parser{
+    private int i = 0;
+    private boolean hayErrores = false;
+    private Token preanalisis;
+    private final List<Token> tokens;
+    public ASA(List<Token> tokens){
+        this.tokens=tokens;
+        preanalisis=this.tokens.get(i);
+    }
+
+    public void analisis(){
+        Stack<String> stack=new Stack<>();
+        stack.push("1");
+        String x=stack.peek();
+        Boolean re=true;
+        while(re){
+            switch (x) {
+                /*
+                -------------------------------------------------------------------
+                CASOS DEL 1 AL 10: HIRAM
+                -------------------------------------------------------------------
+                */
+                case "1":
+                    if(preanalisis.tipo.name()=="SELECT"){
+                        stack.push("SELECT");
+                        stack.push("3");
+                        x=stack.peek();
+                        i++;
+                        preanalisis=this.tokens.get(i);
+                    }
+                    else{
+                         System.out.println("Error: 'select' esperado");
+                         hayErrores=true;
+                         re=false;
+                    }
+                    break;
+
+                case "2":
+                    if(preanalisis.tipo.name()=="EOF"){
+                        hayErrores=false;
+                        re=false;
+                    }
+                    else{
+                        System.out.println("Error");
+                        re=false;
+                    }
+                    break;
+
+                case "3":
+                    if(preanalisis.tipo.name()=="DISTINCT"){
+                        stack.push("DISTINCT");
+                        stack.push("14");
+                        x=stack.peek();
+                    }
+                    else if(preanalisis.tipo.name()=="ASTERISCO"){
+                        stack.push("ASTERISCO");
+                        stack.push("25");
+                        x=stack.peek();
+                    }
+                    else if(preanalisis.tipo.name()=="IDENTIFICADOR"){
+                        stack.push("IDENTIFICADOR");
+                        stack.push("19");
+                        x=stack.peek();
+                    }
+                    else{
+                        System.out.println("Error: 'distinct' o 'asterisco' o 'identificador' esperados");
+                        hayErrores=true;
+                        re=false;
+                    }
+                    i++;
+                    preanalisis=this.tokens.get(i);
+                    break;
+                
+                case "4":
+                    if(preanalisis.tipo.name()=="FROM"){
+                        stack.push("FROM");
+                        stack.push("5");
+                        x=stack.peek();
+                    }
+                    else{
+                        System.out.println("Error: 'from' esperado");
+                        hayErrores=true;
+                        re=false;
+                    }
+                    i++;
+                    preanalisis=this.tokens.get(i);
+                    break;
+
+                case "5":
+                    if(preanalisis.tipo.name()=="IDENTIFICADOR"){
+                        stack.push("IDENTIFICADOR");
+                        stack.push("7");
+                        x=stack.peek();
+                    }
+                    else{
+                        System.out.println("Error: 'id' esperado");
+                        hayErrores=true;
+                        re=false;
+                    }
+                    i++;
+                    preanalisis=this.tokens.get(i);
+                    break;
+
+                case "6":
+                    if(preanalisis.tipo.name()=="COMA"){
+                        stack.push("COMA");
+                        stack.push("8");
+                        x=stack.peek();
+                        i++;
+                        preanalisis=this.tokens.get(i);
+                    }
+                    else if(preanalisis.tipo.name()=="EOF"){
+                        stack.pop();
+                        stack.pop();
+                        stack.push("EOF");
+                        stack.push("2");
+                        x=stack.peek();
+                    }
+                    else{
+                        System.out.println("Error: 'coma' esperado");
+                        hayErrores=true;
+                        re=false;
+                    }
+                    break;
+                
+                case "7":
+                    if(preanalisis.tipo.name()=="IDENTIFICADOR"){
+                        stack.push("IDENTIFICADOR");
+                        stack.push("10");
+                        x=stack.peek();
+                        i++;
+                        preanalisis=this.tokens.get(i);
+                    }
+                    else if(preanalisis.tipo.name()=="COMA"){
+                        stack.pop();
+                        stack.pop();
+                        stack.push("COMA");
+                        stack.push("11");
+                        x=stack.peek();
+                    }
+                    else if(preanalisis.tipo.name()=="EOF"){
+                        stack.pop();
+                        stack.pop();
+                        stack.push("EOF");
+                        stack.push("11");
+                        x=stack.peek();
+                    }
+                    else{
+                        System.out.println("Error: 'id' esperado");
+                        hayErrores=true;
+                        re=false;
+                    }
+                    break;
+                
+                case "8":
+                    if(preanalisis.tipo.name()=="IDENTIFICADOR"){
+                        stack.push("IDENTIFICADOR");
+                        stack.push("7");
+                        x=stack.peek();
+                    }
+                    else{
+                        System.out.println("Error: 'id' esperado");
+                        hayErrores=true;
+                        re=false;
+                    }
+                    i++;
+                    preanalisis=this.tokens.get(i);
+                    break;
+
+                case "10":
+                    stack.pop();
+                    stack.pop();
+                    if(preanalisis.tipo.name()=="COMA"){
+                        stack.push("COMA");
+                        stack.push("11");
+                        x=stack.peek();
+                    }
+                    else if(preanalisis.tipo.name()=="EOF"){
+                        stack.push("EOF");
+                        stack.push("11");
+                        x=stack.peek();
+                    }
+                    else{
+                        System.out.println("Error: 'coma' esperado");
+                        hayErrores=true;
+                        re=false;
+                    }
+                    break;
+                /*
+                -------------------------------------------------------------------
+                CASOS DEL 11 AL 20: RAUL
+                -------------------------------------------------------------------
+                */
+                
+                /*
+                -------------------------------------------------------------------
+                CASOS DEL 20 AL 26: TANIA
+                -------------------------------------------------------------------
+                */
+                
+                
+                default:
+                    System.out.println("Se han encontrado errores");
+                    re=false;
+                    hayErrores=true;
+                    break;
+            }
+        }
+    }
+
+    @Override
+    public boolean parse(){
+        analisis();
+        return !hayErrores;
+    }
+}
